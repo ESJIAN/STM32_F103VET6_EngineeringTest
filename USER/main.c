a61cdefa4fea779f53a1d7f9a0af40bbf09d82ba
ESJIAN <3340580252@qq.com>
2024-12-16 10:30:58 +0800

    feat(CORE): 添加 Cortex-M3 核心外设访问层源文件
    - 新增 core_cm3.c 文件，实现 Cortex-M3 处理器的核心外设访问功能
    - 包含不同编译器（ARMCC、IAR、GCC、TASKING）下的实现
    - 提供栈指针操作、优先级控制、故障掩码、控制寄存器等操作函数
    - 实现数据交换、字节序转换等辅助功能

---
+++ USER/main.c
@@ -0,0 +1,101 @@
+//////////////////////////////////////////////////////////////////////////////////	 
+//本程序只供学习使用，未经作者许可，不得用于其它任何用途
+//中景园电子
+//店铺地址：http://shop73023976.taobao.com/?spm=2013.1.0.0.M4PqC2
+//
+//  文 件 名   : main.c
+//  版 本 号   : v2.0
+//  作    者   : HuangKai
+//  生成日期   : 2014-0101
+//  最近修改   : 
+//  功能描述   : OLED 4接口演示例程(51系列)
+//              说明: 
+//              ----------------------------------------------------------------
+//              GND    电源地
+//              VCC  接5V或3.3v电源
+//              D0   接PB13（SCL）
+//              D1   接PB15（SDA）
+//              RES  接PB11
+//              DC   接PB10
+//              CS   接P12               
+//              ----------------------------------------------------------------
+// 修改历史   :
+// 日    期   : 
+// 作    者   : HuangKai
+// 修改内容   : 创建文件
+//版权所有，盗版必究。
+//Copyright(C) 中景园电子2014/3/16
+//All rights reserved
+//******************************************************************************/
+
+#include "delay.h"
+#include "sys.h"
+#include "oled.h"
+#include "bmp.h"
+#include "key.h"
+#include "led.h"
+ int main(void)
+ {	u8 t;
+		delay_init();	    	 //延时函数初始化	  
+		NVIC_Configuration(); 	 //设置NVIC中断分组2:2位抢占优先级，2位响应优先级 	LED_Init();			     //LED端口初始化
+		OLED_Init();			//初始化OLED  
+		OLED_Clear()  	; 
+	 	LED_Init();		  	 	//初始化与LED连接的硬件接口
+		KEY_Init();          	//初始化与按键连接的硬件接口
+		OLED_Clear();
+//		LED_ON;
+		OLED_ShowCHinese(30,0,0);//元
+		OLED_ShowCHinese(48,0,1);//器
+		OLED_ShowCHinese(66,0,2);//科
+		OLED_ShowCHinese(84,0,3);//技
+		OLED_ShowString(0,3,"0.96' OLED TEST");
+		//OLED_ShowString(8,2,"ZHONGJINGYUAN");  
+	 //	OLED_ShowString(20,4,"2014/05/01");  
+		OLED_ShowString(0,6,"ASCII:");  
+		OLED_ShowString(63,6,"CODE:");
+	
+		LED0=0;
+		LED1=0;
+		while(1)
+		{
+			t=KEY_Scan(0);		//得到键值
+			switch(t)
+			{				 
+				case KEY0_PRES:
+					LED0=!LED0;
+					OLED_Clear();
+					OLED_DrawBMP(0,0,128,8,BMP2);
+					break;
+				case KEY1_PRES:
+					LED1=!LED1;
+					OLED_Clear();
+					OLED_DrawBMP(0,0,128,8,BMP1);
+					break;
+				case KEY2_PRES:
+					LED1=!LED1;
+					OLED_Clear();
+					OLED_DrawBMP(0,0,128,8,BMP2);
+					break;
+				case WKUP_PRES:				
+					LED0=!LED0;
+					LED1=!LED1;
+					OLED_Clear();
+					OLED_ShowCHinese(30,0,0);//元
+					OLED_ShowCHinese(48,0,1);//器
+					OLED_ShowCHinese(66,0,2);//科
+					OLED_ShowCHinese(84,0,3);//技
+					OLED_ShowString(0,3,"0.96' OLED TEST");
+					//OLED_ShowString(8,2,"ZHONGJINGYUAN");  
+				 //	OLED_ShowString(20,4,"2014/05/01");  
+					OLED_ShowString(0,6,"ASCII:");  
+
+					OLED_ShowString(63,6,"CODE:");
+				
+					break;
+				default:
+					delay_ms(10);	
+			} 
+		}	 
+	
+}
+
